1f5d40d033f35131612f46d8925f6446ee831933
ChoYoungwook <34482785+youngwookjo@users.noreply.github.com>
2025-06-17 18:32:00 +0900

    Update ElectronicProduct.js

--- sprintMission1/models/ElectronicProduct.js
+++ sprintMission1/models/ElectronicProduct.js
@@ -8,7 +8,7 @@ export class ElectronicProduct extends Product {
   get manufacturer() {
     return this._manufacturer;
   };
-  //한라인에서 한명령어만 선언해라 if else문 추천 짧은거일떄만 삼항연산자 팀원들과 상의의
+
   set manufacturer(manufacturer) {
     typeof manufacturer === 'string' && manufacturer.length > 0 ? this._manufacturer = manufacturer : this._manufacturer = '제조사 정보가 없습니다';
   };
