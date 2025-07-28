7c9c29c38f229ff341c288d02b3bb42a8801de68
choYoungWook <wwwe2410@naver.com>
2025-07-28 22:48:45 +0900

    import 삭제

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import auth from "../middlewares/auth.js";
 import { validateCreateComment, validatePatchComment } from "../middlewares/validateComment.js";
 import CommentController from "../controllers/commentController.js";
-import CommentService from "../services/commentService.js";
 
 export const productCommentRouter = Router();
 export const freeCommentRouter = Router();
